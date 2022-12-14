dfea2a3adf142f91f33e5a347927ca9b13b71a5d
nicolinalarsson <nicolinalarsson1@gmail.com>
2022-12-14 14:36:11 +0100

    :)

---
+++ src/main/java/teambravo/pipergames/Nicolina/Template.java
@@ -0,0 +1,13 @@
+package teambravo.pipergames.Nicolina;
+
+import javafx.application.Application;
+
+public abstract class Template extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+
+
+    }
+}
